cde64a9b548088d7bf75e3ee4a3d0de147f82ee9
ccontavalli <ccontavalli@gmail.com>
2018-08-22 10:57:30 -0700

    Added object to mock a FileInfo.

---
+++ testing/fileinfo.go
@@ -0,0 +1,40 @@
+package gutesting
+
+import (
+"time"
+"os"
+)
+
+type MockFileInfo struct {
+	NameValue string
+	SizeValue int64
+	ModeValue os.FileMode
+	ModTimeValue time.Time
+	IsDirValue bool
+	SysValue interface{}
+}
+
+func (mfi *MockFileInfo) Name() string {
+	return mfi.NameValue
+}
+
+func (mfi *MockFileInfo) Size() int64 {
+	return mfi.SizeValue
+}
+
+func (mfi *MockFileInfo) Mode() os.FileMode {
+	return mfi.ModeValue
+}
+
+func (mfi *MockFileInfo) ModTime() time.Time {
+	return mfi.ModTimeValue
+}
+
+func (mfi *MockFileInfo) IsDir() bool {
+	return mfi.IsDirValue
+}
+
+func (mfi *MockFileInfo) Sys() interface{} {
+	return mfi.SysValue
+}
+
