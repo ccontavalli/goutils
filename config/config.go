453ad53bfdd97e59872a7a0930239c739067a9ae
ccontavalli <ccontavalli@gmail.com>
2019-02-12 12:12:02 -0800

    Added a couple utility functions to marshal files.

--- config/config.go
+++ config/config.go
@@ -7,6 +7,7 @@ import (
 	"encoding/json"
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
+	"os"
 )
 
 // This is a function able to parse a buffer into any kind of struct, returning
@@ -14,6 +15,8 @@ import (
 //
 // Example of UnmarshalFunction are the standard json.Unmarshal or yaml.Unmarshal.
 type UnmarshalFunction func([]byte, interface{}) error
+
+type MarshalFunction func(interface{}) ([]byte, error)
 
 // Reads a config from a file and unmarshals it.
 //
@@ -34,4 +37,24 @@ func ReadYamlConfigFromFile(filename string, result interface{}) error {
 // Reads a file from disk and parses it as json.
 func ReadJsonConfigFromFile(filename string, result interface{}) error {
 	return ReadMarshaledConfigFromFile(filename, json.Unmarshal, result)
+}
+
+func MarshalConfigToFile(filename string, mode os.FileMode, marshaller MarshalFunction, data interface{}) error {
+	config, err := marshaller(data)
+	if err != nil {
+		return err
+	}
+	err = ioutil.WriteFile(filename, config, mode)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func MarshalJsonConfigToFile(filename string, mode os.FileMode, data interface{}) error {
+	return MarshalConfigToFile(filename, mode, json.Marshal, data)
+}
+
+func MarshalYamlConfigToFile(filename string, mode os.FileMode, data interface{}) error {
+	return MarshalConfigToFile(filename, mode, yaml.Marshal, data)
 }
