c98f4a4b44a231fb33a4d35b46e6befc43d5feae
ccontavalli <ccontavalli@gmail.com>
2019-02-14 13:21:47 -0800

    Add support for list of flags in multiflag library.

---
+++ misc/multiflag.go
@@ -0,0 +1,29 @@
+package misc
+
+import (
+	"flag"
+	"fmt"
+)
+
+type multiFlag struct {
+	array     []string
+	defaulted bool
+}
+
+func (m *multiFlag) String() string {
+	return fmt.Sprintf("%s", m.array)
+}
+
+func (m *multiFlag) Set(value string) error {
+	if m.defaulted {
+		m.array = []string{}
+	}
+	m.array = append(m.array, value)
+	return nil
+}
+
+func MultiString(flagname string, defaults []string, help string) *[]string {
+	toparse := multiFlag{defaults, true}
+	flag.Var(&toparse, flagname, help)
+	return &toparse.array
+}
